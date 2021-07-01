3d992853aeb0508d10c52d0cb26ba64f300534f5
jakob <jakob.germano@gmail.com>
2021-06-30 19:19:36 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//document.addEventListener("DOMContentLoaded", funciton() {
+     //element = document.getElementById("text")
+  // if (element.id = "This is really cool!")
+//});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = "This is really cool!"
+  });
